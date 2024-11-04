fe887930651a86480db5caec1f9b4348d666e379
albertosanclemente <a24albertomt@iessanclemente.net>
2024-11-04 19:33:25 +0100

    cree un metodo en el  header.php para poder usarlo en las distintas paginas

---
+++ www/UD2/entregaTarea/header.php
@@ -0,0 +1,7 @@
+<?php
+function HeaderUD2(){
+    echo '<header class="bg-primary text-white text-center py-3">';
+    echo '<h1>UD2 - Alberto Maneiro Tubio</h1>';
+    echo '<p>Es una tarea de DWCS de la UD2</p>';
+    echo '</header>';
+}
