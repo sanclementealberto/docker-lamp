7f2b85b13f8d5b92d71991159749afd88eb074f4
@albertosanclemente@ <a24albertomt@iessanclemente.net>
2024-11-05 13:28:15 +0100

    asigno a una cookie llamada COOKIETAREAS el valor del array tareas para que persistan los datos

--- www/UD2/entregaTarea/utils.php
+++ www/UD2/entregaTarea/utils.php
@@ -1,6 +1,7 @@
 <?php
 $tareas = [];
 
+
 function guardarTarea($descripcion, $estado)
 {
     global $tareas;
@@ -18,7 +19,18 @@ function guardarTarea($descripcion, $estado)
 
 
     $tareas[] = $nuevaTarea;
-
+    // guardo las tareas en una coockie 86400s=1 dia
+    setcookie('COOKIETAREAS', json_encode($tareas), time() + (86400 * 30), "/"); 
 
     return true;
 }
+
+
+function cargarTareasDesdeCookie() {
+    global $tareas;
+
+    if (isset($_COOKIE['COOKIETAREAS']) && !empty($_COOKIE['COOKIETAREAS'])) {
+        $tareas = json_decode($_COOKIE['COOKIETAREAS'], true);
+    }
+}
+cargarTareasDesdeCookie();
