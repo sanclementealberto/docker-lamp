1642da5e140b06f81fa3368d7c32c4b0a4849e1d
albertosanclemente <a24albertomt@iessanclemente.net>
2024-11-05 01:09:30 +0100

    movi utils

--- www/UD2/entregaTarea/nueva.php
+++ www/UD2/entregaTarea/nueva.php
@@ -1,8 +1,3 @@
-<?php include './utils.php';
-
-$mensaje = obtenerDatosForm();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,44 +6,49 @@ $mensaje = obtenerDatosForm();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nueva Tarea</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-   
+
 </head>
 
 <body>
-   
 
-<?php
-   
 
-    function obtenerDatosForm() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $descripcion = trim($_POST['descripcion']);
-            $estado = trim($_POST['estado']);
+    <?php
+    include './utils.php';
 
-            if (!empty($descripcion) && !empty($estado)) {
-                $resultado = guardarTarea($descripcion, $estado);
-                
-                return $resultado ? true :false ;
-            } 
-        }
-        return null;
+    $mensaje = obtenerDatosForm();
+    
+
+    function obtenerDatosForm(): bool|null
+    {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $descripcion = trim($_POST['descripcion']);
+    $estado = trim($_POST['estado']);
+
+    if (!empty($descripcion) && !empty($estado)) {
+    $resultado = guardarTarea($descripcion, $estado);
+
+    return $resultado ? true : false;
+    }
+    }
+    return null;
     }
 
-    function mostrarMensaje($mensaje) {
-        if ($mensaje === true) {
-            return '<div class="alert alert-success" role="alert"> La tarea se ha guardado correctamente.</div>';
-        } elseif ($mensaje === false) {
-            return '<div class="alert alert-danger" role="alert"> Error al guardar la tarea. Verifique los datos.</div>';
-        
-        }
+    function mostrarMensaje($mensaje)
+    {
+    if ($mensaje === true) {
+    return '<div class="alert alert-success" role="alert"> La tarea se ha guardado correctamente.</div>';
+    } elseif ($mensaje === false) {
+    return '<div class="alert alert-danger" role="alert"> Error al guardar la tarea. Verifique los datos.</div>';
     }
+    }
+  
     ?>
 
     <div class="container mt-4">
         <?php echo mostrarMensaje($mensaje); ?>
-        
+
     </div>
-    
+
 </body>
 
 </html>
