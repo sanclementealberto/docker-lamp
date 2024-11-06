db3596507acdb154a65361efcb6a3810a90f0576
@albertosanclemente@ <a24albertomt@iessanclemente.net>
2024-11-06 14:55:48 +0100

    finalizados os ejercicios

---
+++ www/UD2/ejercicios/introduccion/tarea6.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<em> hola mundo <em> <br/>";
+    $tu_nombre="alberto maneiro tubio";
+    echo "<b> bienvenido ".$tu_nombre. "</b>";
+
+
+    ?>
+</body>
+</html>
