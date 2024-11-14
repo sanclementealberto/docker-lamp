9f9f3a6e6b907d40461820a1c51c0e2479fb82ee
@albertosanclemente@ <a24albertomt@iessanclemente.net>
2024-11-14 14:31:04 +0100

    cambios en header

--- www/UD3/ejercicios/tienda/header.php
+++ www/UD3/ejercicios/tienda/header.php
@@ -2,7 +2,7 @@
 
     function headerTienda(){
         return <<<HTML
-        <div class="container-fluid bg-success  py-2">
+        <div class="container-fluid bg-success w py-2">
           
             <header class="text-center">
                 <h1>Bienvenidos a la Tienda de Recambios</h1>
